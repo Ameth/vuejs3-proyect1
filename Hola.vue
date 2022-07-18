ea638b258294ad2bb198ae4dbcc9b6029769170c
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-07-18 15:49:09 -0500

    create project with vue-cli

---
+++ Hola.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <p>{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: "Hola Vue!"
+        }
+    }
+}
+</script>
+
+<style scoped>
+p {
+    color: red;
+}
+</style>
---
+++ hola-vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hola Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+
+p:nth-child(2) {
+  color: blue;
+}
+</style>
